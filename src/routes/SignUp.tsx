493b552145a96b9d41b9f5ef9a82d432e5516740
Sangmin Lee <gink.ss00@gmail.com>
2023-02-02 15:23:22 +0900

    feat: 회원가입 유효성 검사 구현
    이메일과 비밀번호를 입력받는다.
    제한 조건에 맞지 않으면 버튼이 비활성화 된다.
    
    제한 조건)
    - 이메일: @가 필수로 들어가야함
    - 비밀번호: 8자리 이상이어야 함

--- src/routes/SignUp.tsx
+++ src/routes/SignUp.tsx
@@ -11,21 +11,37 @@ const SignUpButton = styled.button``;
 function SignUp() {
   const [userEmail, setUserEmail] = useState('');
   const [userPassword, setUserPassword] = useState('');
+  const [validation, setValidation] = useState(false);
+
+  useEffect(() => {
+    userEmail.includes('@') && userPassword.length >= 8
+      ? setValidation(true)
+      : setValidation(false);
+  }, [userEmail, userPassword]);
 
   return (
     <SignUpForm>
       <SignUpInput
         data-testid="email-input"
         value={userEmail}
-        onChange={({ currentTarget: { value } }) => setUserEmail(value)}
+        onChange={({ currentTarget: { value } }: React.ChangeEvent<HTMLInputElement>) =>
+          setUserEmail(value)
+        }
       />
       <SignUpInput
         data-testid="password-input"
         type="password"
         value={userPassword}
-        onChange={({ currentTarget: { value } }) => setUserPassword(value)}
+        onChange={({ currentTarget: { value } }: React.ChangeEvent<HTMLInputElement>) =>
+          setUserPassword(value)
+        }
       />
-      <SignUpButton data-testid="signup-button">회원가입</SignUpButton>
+      <SignUpButton
+        data-testid="signup-button"
+        disabled={!validation}
+      >
+        회원가입
+      </SignUpButton>
     </SignUpForm>
   );
 }
