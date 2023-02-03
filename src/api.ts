bf1d54e739fe5feb261e2d4abc6fd4e84326207d
Sangmin Lee <gink.ss00@gmail.com>
2023-02-03 23:01:04 +0900

    feat: 로그인 페이지에서 로그인 기능 구현
    로그인이 성공했을 시 JWT를 받고 로컬 스토리지에 저장한다.
    또한, /todo 경로로 이동한다.
    
    로그인 관련 기타 유효성 검사 기능 등은 회원가입과 같다.

--- src/api.ts
+++ src/api.ts
@@ -12,3 +12,14 @@ export function fetchSignUp({ userEmail, userPassword }: ISignInput) {
     headers: { 'Content-Type': 'application/json' },
   }).then((res) => res.json());
 }
+
+export function fetchSignIn({ userEmail, userPassword }: ISignInput) {
+  return fetch(`${BASE_URL}/auth/signin`, {
+    method: 'POST',
+    body: JSON.stringify({
+      email: userEmail,
+      password: userPassword,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  }).then((res) => res.json());
+}
--- src/routes/SignIn.tsx
+++ src/routes/SignIn.tsx
@@ -1,27 +1,62 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
+import { fetchSignIn } from '../api';
+import { ISignResponse } from '../type';
+import { signValidation } from '../validator';
 
-const SignInForm = styled.form`
+const SignForm = styled.form`
   display: flex;
   flex-direction: column;
 `;
-const SignInInput = styled.input``;
-const SignInButton = styled.button``;
+const SignInput = styled.input``;
+const SignButton = styled.button``;
 
 function SignIn() {
   const [userEmail, setUserEmail] = useState('');
   const [userPassword, setUserPassword] = useState('');
+  const [onValid, setOnValid] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    setOnValid(signValidation({ userEmail, userPassword }));
+  }, [userEmail, userPassword]);
+
+  const onClickEffect = async () => {
+    const signInResponse: ISignResponse = await fetchSignIn({
+      userEmail,
+      userPassword,
+    });
+
+    if (signInResponse.access_token) {
+      localStorage.setItem('wantedAccessToken', signInResponse.access_token);
+      history.replace('/todo');
+    }
+  };
 
   return (
-    <SignInForm>
-      <SignInInput
+    <SignForm
+      onSubmit={(event: React.FormEvent<HTMLFormElement>) => event.preventDefault()}
+    >
+      <SignInput
         data-testid="email-input"
         value={userEmail}
-        onChange={({ currentTarget: { value } }) => setUserEmail(value)}
+        onChange={({ currentTarget: { value } }: React.ChangeEvent<HTMLInputElement>) =>
+          setUserEmail(value)
+        }
       />
-      <SignInInput data-testid="password-input" type="password" value={userPassword} />
-      <SignInButton data-testid="signin-button">로그인</SignInButton>
-    </SignInForm>
+      <SignInput
+        data-testid="password-input"
+        type="password"
+        value={userPassword}
+        onChange={({ currentTarget: { value } }: React.ChangeEvent<HTMLInputElement>) =>
+          setUserPassword(value)
+        }
+      />
+      <SignButton data-testid="signin-button" disabled={!onValid} onClick={onClickEffect}>
+        로그인
+      </SignButton>
+    </SignForm>
   );
 }
 
